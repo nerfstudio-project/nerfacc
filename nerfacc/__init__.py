b73fa67b5660cec35e4d393c5bc30f8bcde016a9
Ruilong Li <ruilongli94@gmail.com>
2022-09-13 20:03:27 +0000

    expose utils functions

--- nerfacc/__init__.py
+++ nerfacc/__init__.py
@@ -1,2 +1,9 @@
 from .occupancy_field import OccupancyField
+from .utils import (
+    ray_aabb_intersect,
+    volumetric_marching,
+    volumetric_rendering_accumulate,
+    volumetric_rendering_steps,
+    volumetric_rendering_weights,
+)
 from .volumetric_rendering import volumetric_rendering
