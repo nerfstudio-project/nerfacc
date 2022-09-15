ec0bd62554de366344b56b4743a6c000c0b38aab
Ruilong Li <ruilongli94@gmail.com>
2022-09-15 04:57:06 +0000

    prettier doc

--- nerfacc/utils.py
+++ nerfacc/utils.py
@@ -16,11 +16,11 @@ def ray_aabb_intersect(
     Args:
         rays_o: Ray origins. Tensor with shape (n_rays, 3).
         rays_d: Normalized ray directions. Tensor with shape (n_rays, 3).
-        aabb: Scene bounding box {xmin, ymin, zmin, xmax, ymax, zmax}.
+        aabb: Scene bounding box {xmin, ymin, zmin, xmax, ymax, zmax}. \
             Tensor with shape (6)
 
     Returns:
-        Ray AABB intersection {t_min, t_max} with shape (n_rays) respectively.
+        Ray AABB intersection {t_min, t_max} with shape (n_rays) respectively. \
         Note the t_min is clipped to minimum zero. 1e10 means no intersection.
 
     """
@@ -52,26 +52,28 @@ def volumetric_marching(
     Args:
         rays_o: Ray origins. Tensor with shape (n_rays, 3).
         rays_d: Normalized ray directions. Tensor with shape (n_rays, 3).
-        aabb: Scene bounding box {xmin, ymin, zmin, xmax, ymax, zmax}.
-        Tensor with shape (6)
+        aabb: Scene bounding box {xmin, ymin, zmin, xmax, ymax, zmax}. \
+            Tensor with shape (6)
         scene_resolution: Shape of the `scene_occ_binary`. {resx, resy, resz}.
-        scene_occ_binary: Scene occupancy binary field. BoolTensor with shape
-        (resx * resy * resz)
-        t_min: Optional. Ray near planes. Tensor with shape (n_ray,).
-        If not given it will be calculated using aabb test. Default is None.
-        t_max: Optional. Ray far planes. Tensor with shape (n_ray,)
-        If not given it will be calculated using aabb test. Default is None.
+        scene_occ_binary: Scene occupancy binary field. BoolTensor with \
+            shape (resx * resy * resz)
+        t_min: Optional. Ray near planes. Tensor with shape (n_ray,). \
+            If not given it will be calculated using aabb test. Default is None.
+        t_max: Optional. Ray far planes. Tensor with shape (n_ray,). \
+            If not given it will be calculated using aabb test. Default is None.
         render_step_size: Marching step size. Default is 1e-3.
 
     Returns:
-        packed_info: Stores infomation on which samples belong to the same ray.
-        It is a tensor with shape (n_rays, 2). For each ray, the two values
-        indicate the start index and the number of samples for this ray,
-        respectively.
-        frustum_origins: Sampled frustum origins. Tensor with shape (n_samples, 3).
-        frustum_dirs: Sampled frustum directions. Tensor with shape (n_samples, 3).
-        frustum_starts: Sampled frustum starts. Tensor with shape (n_samples, 1).
-        frustum_ends: Sampled frustum ends. Tensor with shape (n_samples, 1).
+        A tuple of tensors containing
+
+            - **packed_info**: Stores infomation on which samples belong to the same ray. \
+                It is a tensor with shape (n_rays, 2). For each ray, the two values \
+                indicate the start index and the number of samples for this ray, \
+                respectively.
+            - **frustum_origins**: Sampled frustum origins. Tensor with shape (n_samples, 3).
+            - **frustum_dirs**: Sampled frustum directions. Tensor with shape (n_samples, 3).
+            - **frustum_starts**: Sampled frustum directions. Tensor with shape (n_samples, 3).
+            - **frustum_ends**: Sampled frustum directions. Tensor with shape (n_samples, 3).
 
     """
     if not rays_o.is_cuda:
@@ -122,26 +124,28 @@ def volumetric_rendering_steps(
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """Compute rendering marching steps.
 
-    This function will compact the samples by terminate the marching once the
-    transmittance reaches to 0.9999. It is recommanded that before running your
-    network with gradients enabled, first run this function without gradients
+    This function will compact the samples by terminate the marching once the \
+    transmittance reaches to 0.9999. It is recommanded that before running your \
+    network with gradients enabled, first run this function without gradients \
     (torch.no_grad()) to quickly filter out some samples.
 
     Note: this function is not differentiable to inputs.
 
     Args:
-        packed_info: Stores infomation on which samples belong to the same ray.
-        See volumetric_marching for details. Tensor with shape (n_rays, 2).
-        sigmas: Densities at those samples. Tensor with shape (n_samples, 1).
-        frustum_starts: Where the frustum-shape sample starts along a ray. Tensor with
-        shape (n_samples, 1).
-        frustum_ends: Where the frustum-shape sample ends along a ray. Tensor with
-        shape (n_samples, 1).
+        packed_info: Stores infomation on which samples belong to the same ray. \
+            See volumetric_marching for details. Tensor with shape (n_rays, 2). \
+            sigmas: Densities at those samples. Tensor with shape (n_samples, 1).
+        frustum_starts: Where the frustum-shape sample starts along a ray. Tensor with \
+            shape (n_samples, 1).
+        frustum_ends: Where the frustum-shape sample ends along a ray. Tensor with \
+            shape (n_samples, 1).
 
     Returns:
-        compact_packed_info: Compacted version of input packed_info.
-        compact_frustum_starts: Compacted version of input frustum_starts.
-        compact_frustum_ends: Compacted version of input frustum_ends.
+        A tuple of tensors containing
+
+            **compact_packed_info**: Compacted version of input packed_info.
+            **compact_frustum_starts**: Compacted version of input frustum_starts.
+            **compact_frustum_ends**: Compacted version of input frustum_ends.
 
     """
     if (
@@ -181,18 +185,19 @@ def volumetric_rendering_weights(
     Note: this function is only differentiable to `sigmas`.
 
     Args:
-        packed_info: Stores infomation on which samples belong to the same ray.
-        See volumetric_marching for details. Tensor with shape (n_rays, 2).
+        packed_info: Stores infomation on which samples belong to the same ray. \
+            See ``volumetric_marching`` for details. Tensor with shape (n_rays, 2).
         sigmas: Densities at those samples. Tensor with shape (n_samples, 1).
-        frustum_starts: Where the frustum-shape sample starts along a ray. Tensor with
-        shape (n_samples, 1).
-        frustum_ends: Where the frustum-shape sample ends along a ray. Tensor with
-        shape (n_samples, 1).
+        frustum_starts: Where the frustum-shape sample starts along a ray. Tensor with \
+            shape (n_samples, 1).
+        frustum_ends: Where the frustum-shape sample ends along a ray. Tensor with \
+            shape (n_samples, 1).
 
     Returns:
-        weights: Volumetric rendering weights for those samples. Tensor with shape
-        (n_samples).
-        ray_indices: Ray index of each sample. IntTensor with shape (n_sample).
+        A tuple of tensors containing
+
+            - **weights**: Volumetric rendering weights for those samples. Tensor with shape (n_samples).
+            - **ray_indices**: Ray index of each sample. IntTensor with shape (n_sample).
 
     """
     if (
@@ -224,18 +229,18 @@ def volumetric_rendering_accumulate(
     Note: this function is only differentiable to weights and values.
 
     Args:
-        weights: Volumetric rendering weights for those samples. Tensor with shape
+        weights: Volumetric rendering weights for those samples. Tensor with shape \
             (n_samples).
         ray_indices: Ray index of each sample. IntTensor with shape (n_sample).
-        values: The values to be accmulated. Tensor with shape (n_samples, D). If
+        values: The values to be accmulated. Tensor with shape (n_samples, D). If \
             None, the accumulated values are just weights. Default is None.
-        n_rays: Total number of rays. This will decide the shape of the ouputs. If
-            None, it will be inferred from `ray_indices.max() + 1`.  If specified
+        n_rays: Total number of rays. This will decide the shape of the ouputs. If \
+            None, it will be inferred from `ray_indices.max() + 1`.  If specified \
             it should be at least larger than `ray_indices.max()`. Default is None.
 
     Returns:
-        Accumulated values with shape (n_rays, D). If `values` is not given then
-            we return the accumulated weights, in which case D == 1.
+        Accumulated values with shape (n_rays, D). If `values` is not given then we return \
+            the accumulated weights, in which case D == 1.
     """
     assert ray_indices.dim() == 1 and weights.dim() == 1
     if not weights.is_cuda:
