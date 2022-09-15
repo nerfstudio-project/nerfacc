f5b20d8222d9ce81fdc78ed17b0778d5b0a5d373
Ruilong Li <ruilongli94@gmail.com>
2022-09-15 01:22:59 +0000

    build doc should wokr

--- nerfacc/__init__.py
+++ nerfacc/__init__.py
@@ -7,3 +7,13 @@ from .utils import (
     volumetric_rendering_weights,
 )
 from .volumetric_rendering import volumetric_rendering
+
+__all__ = [
+    "OccupancyField",
+    "ray_aabb_intersect",
+    "volumetric_marching",
+    "volumetric_rendering_accumulate",
+    "volumetric_rendering_steps",
+    "volumetric_rendering_weights",
+    "volumetric_rendering",
+]
