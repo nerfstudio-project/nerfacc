98f820b18c4e71c610932d085b069f15a2b7d553
Ruilong Li(李瑞龙) <ruilongli94@gmail.com>
2022-09-16 08:14:07 -0700

    swith to pytorch theme (#13)
    * swith to pytorch theme
    
    * doc deps

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 
+import pytorch_sphinx_theme
+
 # -- Project information
 
 project = "nerfacc"
@@ -30,7 +32,60 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = "furo"
+# html_theme = "furo"
+
+html_theme = "pytorch_sphinx_theme"
+html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
+html_static_path = ["_static"]
+html_css_files = ["css/readthedocs.css"]
+
+# Ignore >>> when copying code
+copybutton_prompt_text = r">>> |\.\.\. "
+copybutton_prompt_is_regexp = True
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    # The target url that the logo directs to. Unset to do nothing
+    "logo_url": "https://plenoptix-nerfacc.readthedocs-hosted.com/en/latest/index.html",
+    # "menu" is a list of dictionaries where you can specify the content and the
+    # behavior of each item in the menu. Each item can either be a link or a
+    # dropdown menu containing a list of links.
+    "menu": [
+        # A link
+        {"name": "GitHub", "url": "https://github.com/KAIR-BAIR/nerfacc"},
+        # A dropdown menu
+        {
+            "name": "Projects",
+            "children": [
+                # A vanilla dropdown item
+                {
+                    "name": "NeRFactory",
+                    "url": "https://plenoptix-nerfactory.readthedocs-hosted.com/en/latest/?badge=latest",
+                    "description": "The all-in-one repo for NeRFs",
+                },
+            ],
+            # Optional, determining whether this dropdown menu will always be
+            # highlighted.
+            # "active": True,
+        },
+    ],
+}
+# html_theme_options = {
+#     "canonical_url": "",
+#     "analytics_id": "",
+#     "logo_only": False,
+#     "display_version": True,
+#     "prev_next_buttons_location": "bottom",
+#     "style_external_links": False,
+#     # Toc options
+#     "collapse_navigation": True,
+#     "sticky_navigation": True,
+#     "navigation_depth": 4,
+#     "includehidden": True,
+#     "titles_only": False
+# }
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
