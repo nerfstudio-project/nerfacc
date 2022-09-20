d78e6677144c037876a983ed5e90cb15dc26612f
Ruilong Li <ruilongli94@gmail.com>
2022-09-20 06:32:39 +0000

    update test

--- tests/test_marching.py
+++ tests/test_marching.py
@@ -7,8 +7,9 @@ device = "cuda:0"
 
 
 def test_marching():
+    torch.manual_seed(42)
     scene_aabb = torch.tensor([0, 0, 0, 1, 1, 1], device=device).float()
-    scene_occ_binary = torch.ones((128 * 128 * 128), device=device).bool()
+    scene_occ_binary = torch.rand((128 * 128 * 128), device=device) > 0.5
     rays_o = torch.rand((10000, 3), device=device)
     rays_d = torch.randn((10000, 3), device=device)
     rays_d = rays_d / rays_d.norm(dim=-1, keepdim=True)
