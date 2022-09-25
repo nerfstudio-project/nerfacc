e3978b8fffdc4a54388be02e8ff884fc5d3e88a3
Ruilong Li <ruilongli94@gmail.com>
2022-09-25 03:03:24 +0000

    rendering is indeed faster

--- nerfacc/rendering.py
+++ nerfacc/rendering.py
@@ -1,14 +1,81 @@
-from typing import Tuple
+from typing import Optional, Tuple
 
 import torch
+from torch import Tensor
 
 import nerfacc.cuda2 as nerfacc_cuda
 
 
-def transmittance_compress(
+def accumulate_along_rays(
+    weights: Tensor,
+    ray_indices: Tensor,
+    values: Optional[Tensor] = None,
+    n_rays: Optional[int] = None,
+) -> Tensor:
+    """Accumulate volumetric values along the ray.
+
+    Note:
+        this function is only differentiable to `weights` and `values`.
+
+    Args:
+        weights: Volumetric rendering weights for those samples. Tensor with shape \
+            (n_samples).
+        ray_indices: Ray index of each sample. IntTensor with shape (n_sample). \
+            It can be obtained from `unpack_to_ray_indices(packed_info)`.
+        values: The values to be accmulated. Tensor with shape (n_samples, D). If \
+            None, the accumulated values are just weights. Default is None.
+        n_rays: Total number of rays. This will decide the shape of the ouputs. If \
+            None, it will be inferred from `ray_indices.max() + 1`.  If specified \
+            it should be at least larger than `ray_indices.max()`. Default is None.
+
+    Returns:
+        Accumulated values with shape (n_rays, D). If `values` is not given then we return \
+            the accumulated weights, in which case D == 1.
+    """
+    assert ray_indices.dim() == 1 and weights.dim() == 1
+    if not weights.is_cuda:
+        raise NotImplementedError("Only support cuda inputs.")
+    if values is not None:
+        assert values.dim() == 2 and values.shape[0] == weights.shape[0]
+        src = weights[:, None] * values
+    else:
+        src = weights[:, None]
+
+    if ray_indices.numel() == 0:
+        assert n_rays is not None
+        return torch.zeros((n_rays, src.shape[-1]), device=weights.device)
+
+    if n_rays is None:
+        n_rays = int(ray_indices.max()) + 1
+    else:
+        assert n_rays > ray_indices.max()
+
+    index = ray_indices[:, None].long().expand(-1, src.shape[-1])
+    outputs = torch.zeros((n_rays, src.shape[-1]), device=weights.device)
+    outputs.scatter_add_(0, index, src)
+    return outputs
+
+
+def transmittance_compression(
     packed_info, t_starts, t_ends, sigmas, early_step_eps: float = 1e-4
 ) -> Tuple[torch.Tensor, ...]:
-    """Compacts the rendering steps."""
+    """Compress the samples based on the transmittance (early stoping).
+
+    Args:
+        packed_info: Stores information on which samples belong to the same ray. \
+            See :func:`nerfacc.ray_marching` for details. Tensor with shape (n_rays, 2).
+        t_starts: Where the frustum-shape sample starts along a ray. Tensor with \
+            shape (n_samples, 1).
+        t_ends: Where the frustum-shape sample ends along a ray. Tensor with \
+            shape (n_samples, 1).
+        sigmas: The sigma values of the samples. Tensor with shape (n_samples, 1).
+        early_step_eps: The epsilon value for early stopping. Default is 1e-4.
+    
+    Returns:
+        A tuple of 5 tensors. The first 4 tensors are the compacted {packed_info, \
+        t_starts, t_ends, sigmas}. The last tensor is the compacted weights.
+
+    """
     # compute weights and compact samples
     (
         _packed_info,
@@ -16,20 +83,21 @@ def transmittance_compress(
         _t_ends,
         _sigmas,
         _weights,
-    ) = _compacted_rendering_forward(
+    ) = _transmittance_compression_forward(
         packed_info, t_starts, t_ends, sigmas, early_step_eps
     )
     # register backward: weights -> sigmas.
-    _weights = _CompactedRenderingBackward.apply(
+    _weights = _TransmittanceCompressionBackward.apply(
         _packed_info, _t_starts, _t_ends, _sigmas, _weights, early_step_eps
     )
     # return compacted results.
     return _packed_info, _t_starts, _t_ends, _sigmas, _weights
 
 
-def _compacted_rendering_forward(
+def _transmittance_compression_forward(
     packed_info, t_starts, t_ends, sigmas, early_step_eps: float = 1e-4
 ):
+    """Forward pass of the transmittance compression."""
     with torch.no_grad():
         weights, _packed_info, compact_selector = nerfacc_cuda.rendering_forward(
             packed_info.contiguous(),
@@ -45,7 +113,9 @@ def _compacted_rendering_forward(
     return _packed_info, _t_starts, _t_ends, _sigmas, _weights
 
 
-class _CompactedRenderingBackward(torch.autograd.Function):
+class _TransmittanceCompressionBackward(torch.autograd.Function):
+    """Backward pass of the transmittance compression."""
+
     @staticmethod
     def forward(
         ctx,
@@ -56,14 +126,15 @@ class _CompactedRenderingBackward(torch.autograd.Function):
         _weights,
         early_step_eps: float = 1e-4,
     ):
-        ctx.save_for_backward(
-            _packed_info,
-            _t_starts,
-            _t_ends,
-            _sigmas,
-            _weights,
-        )
-        ctx.early_step_eps = early_step_eps
+        if ctx.needs_input_grad[3]:  # sigmas
+            ctx.save_for_backward(
+                _packed_info,
+                _t_starts,
+                _t_ends,
+                _sigmas,
+                _weights,
+            )
+            ctx.early_step_eps = early_step_eps
         return _weights
 
     @staticmethod
--- tests/compare.py
+++ tests/compare.py
@@ -1,4 +1,5 @@
 import torch
+import tqdm
 
 from nerfacc.cuda import (
     volumetric_rendering_steps,
@@ -6,10 +7,10 @@ from nerfacc.cuda import (
     volumetric_rendering_weights_forward,
 )
 from nerfacc.ray_marching import ray_marching
-from nerfacc.rendering import transmittance_compress
+from nerfacc.rendering import transmittance_compression
 
 device = "cuda:0"
-batch_size = 128
+batch_size = 1280
 
 rays_o = torch.rand((batch_size, 3), device=device)
 rays_d = torch.randn((batch_size, 3), device=device)
@@ -23,86 +24,76 @@ packed_info, t_starts, t_ends = ray_marching(
     render_step_size=1e-2,
 )
 torch.manual_seed(42)
-sigmas = torch.rand_like(t_starts, requires_grad=True)
-_packed_info, _t_starts, _t_ends, _sigmas, _weights = transmittance_compress(
-    packed_info,
-    t_starts,
-    t_ends,
-    sigmas * 1e2,
-)
-_weights.sum().backward()
+
+for _ in tqdm.tqdm(range(5000)):
+    sigmas = torch.rand_like(t_starts, requires_grad=True)
+    _packed_info, _t_starts, _t_ends, _sigmas, _weights = transmittance_compression(
+        packed_info,
+        t_starts,
+        t_ends,
+        sigmas * 1e2,
+    )
+    _weights.sum().backward()
 # print("weights", _weights.abs().sum(), _weights.shape)
 # print("grad", sigmas.grad.abs().sum(), sigmas.shape)
 
 torch.manual_seed(42)
-sigmas = torch.rand_like(t_starts, requires_grad=True)
 
-compact_packed_info, compact_selector = volumetric_rendering_steps(
-    packed_info,
-    t_starts,
-    t_ends,
-    sigmas * 1e2,
-)
+for _ in tqdm.tqdm(range(5000)):
+    sigmas = torch.rand_like(t_starts, requires_grad=True)
 
+    compact_packed_info, compact_selector = volumetric_rendering_steps(
+        packed_info,
+        t_starts,
+        t_ends,
+        sigmas * 1e2,
+    )
 
-class _VolumetricRenderingWeights(torch.autograd.Function):
-    @staticmethod
-    def forward(
-        ctx, packed_info, frustum_starts, frustum_ends, sigmas
-    ):  # pylint: disable=arguments-differ
-        weights = volumetric_rendering_weights_forward(
-            packed_info.contiguous(),
-            frustum_starts.contiguous(),
-            frustum_ends.contiguous(),
-            sigmas.contiguous(),
-        )
-        ctx.save_for_backward(
-            packed_info,
-            frustum_starts,
-            frustum_ends,
-            sigmas,
-            weights,
-        )
-        return weights
+    class _VolumetricRenderingWeights(torch.autograd.Function):
+        @staticmethod
+        def forward(
+            ctx, packed_info, frustum_starts, frustum_ends, sigmas
+        ):  # pylint: disable=arguments-differ
+            weights = volumetric_rendering_weights_forward(
+                packed_info.contiguous(),
+                frustum_starts.contiguous(),
+                frustum_ends.contiguous(),
+                sigmas.contiguous(),
+            )
+            ctx.save_for_backward(
+                packed_info,
+                frustum_starts,
+                frustum_ends,
+                sigmas,
+                weights,
+            )
+            return weights
 
-    @staticmethod
-    def backward(ctx, grad_weights):  # pylint: disable=arguments-differ
-        (
-            packed_info,
-            frustum_starts,
-            frustum_ends,
-            sigmas,
-            weights,
-        ) = ctx.saved_tensors
-        grad_sigmas = volumetric_rendering_weights_backward(
-            weights.contiguous(),
-            grad_weights.contiguous(),
-            packed_info.contiguous(),
-            frustum_starts.contiguous(),
-            frustum_ends.contiguous(),
-            sigmas.contiguous(),
-        )
-        assert torch.allclose(_sigmas, sigmas)
-        assert torch.allclose(_t_starts, frustum_starts)
-        assert torch.allclose(_t_ends, frustum_ends)
-        assert torch.allclose(_packed_info, packed_info)
-        assert torch.allclose(_weights, weights)
-        print("grad_sigmas", grad_sigmas.shape, grad_sigmas.abs().sum())
-        print("grad_weights", grad_weights.shape, grad_weights.abs().sum())
-        # print("-----")weights
-        # print("sigmas", sigmas.sum(), sigmas.shape)
-        # print("t_starts", frustum_starts.sum(), frustum_starts.shape)
-        # print("grad_weights", grad_weights.sum(), grad_weights.shape)
-        # print("grad_sigmas", grad_sigmas.sum(), grad_sigmas.shape)
-        return None, None, None, grad_sigmas
+        @staticmethod
+        def backward(ctx, grad_weights):  # pylint: disable=arguments-differ
+            (
+                packed_info,
+                frustum_starts,
+                frustum_ends,
+                sigmas,
+                weights,
+            ) = ctx.saved_tensors
+            grad_sigmas = volumetric_rendering_weights_backward(
+                weights.contiguous(),
+                grad_weights.contiguous(),
+                packed_info.contiguous(),
+                frustum_starts.contiguous(),
+                frustum_ends.contiguous(),
+                sigmas.contiguous(),
+            )
+            return None, None, None, grad_sigmas
 
-
-_weights = _VolumetricRenderingWeights.apply(
-    compact_packed_info.contiguous(),
-    t_starts[compact_selector].contiguous(),
-    t_ends[compact_selector].contiguous(),
-    sigmas[compact_selector].contiguous() * 1e2,
-)
-_weights.sum().backward()
+    _weights = _VolumetricRenderingWeights.apply(
+        compact_packed_info.contiguous(),
+        t_starts[compact_selector].contiguous(),
+        t_ends[compact_selector].contiguous(),
+        sigmas[compact_selector].contiguous() * 1e2,
+    )
+    _weights.sum().backward()
 # print("weights", _weights.abs().sum(), _weights.shape)
 # print("grad", sigmas.grad.abs().sum(), sigmas.shape)
