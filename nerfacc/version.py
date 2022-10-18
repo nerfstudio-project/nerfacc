934f5e9fcc6cd1d56b886ff7356fb99e49379d3a
Matthew Tancik <mattt4001@gmail.com>
2022-10-18 12:55:42 -0700

    Support python 3.7 (#77)
    * Support python 3.7
    
    * Add dependency

--- nerfacc/version.py
+++ nerfacc/version.py
@@ -1,6 +1,10 @@
 """
 Copyright (c) 2022 Ruilong Li, UC Berkeley.
 """
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    from importlib_metadata import version
 
 __version__ = version("nerfacc")
