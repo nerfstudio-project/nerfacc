07b94fb1e0b80815a6bbb2114fdb364faedd058b
Semih Dinc <semih.dinc@eagleview.com>
2022-11-28 21:23:47 +0000

    added checkpoints, render only option

--- examples/train_ngp_nerf.py
+++ examples/train_ngp_nerf.py
@@ -6,6 +6,7 @@ import argparse
 import math
 import os
 import time
+import glob
 
 import imageio
 import numpy as np
@@ -27,24 +28,26 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--exp_name",type=str,default="exp",help="The name of the folder for saving results")
     parser.add_argument("--scene",type=str,default="lego",help="which scene to use")
-    parser.add_argument("--aabb",type=lambda s: [float(item) for item in s.split(",")],default="-1.5,-1.5,-1.5,1.5,1.5,1.5",help="delimited list input")
     parser.add_argument("--test_chunk_size",type=int,default=8192)
-    parser.add_argument("--ev_data",action="store_true",help="whether to use EV dataset or not")
-    parser.add_argument("--unbounded",action="store_true",help="whether to use unbounded rendering")
-    parser.add_argument("--auto_aabb",action="store_true",help="whether to automatically compute the aabb")
+    parser.add_argument("--max_steps",type=int,default=30000, help="Max number of training iterations")
     parser.add_argument("--cone_angle", type=float, default=0.0)
-    parser.add_argument("--i_test",type=int, default=5000)
+    parser.add_argument("--i_ckpt",type=int, default=5000, help="Iterations to save model")
+    parser.add_argument("--render_only",action="store_true",help="whether to only render images")
+    
+    #currently useless options
+    parser.add_argument("--i_test",type=int, default=5000, help="Iterations to render test poses and create video") 
+    parser.add_argument("--aabb",type=lambda s: [float(item) for item in s.split(",")],default="-1.5,-1.5,-1.5,1.5,1.5,1.5",help="delimited list input")
+    
     args = parser.parse_args()
 
     render_n_samples = 1024
 
     #---------------------------------------------------------------------------------------------------------------------------------------
-    if args.ev_data == True:
-        from datasets.nerf_synthetic import SubjectLoader
-        from datasets.generateTestPoses import SubjectTestPoseLoader
-        data_root_fp = "/home/ubuntu/data/"
-        target_sample_batch_size = 1 << 20
-        grid_resolution = 256
+    from datasets.nerf_synthetic import SubjectLoader
+    from datasets.generateTestPoses import SubjectTestPoseLoader
+    data_root_fp = "/home/ubuntu/data/"
+    target_sample_batch_size = 1 << 20
+    grid_resolution = 256
 
     #---------------------------------------------------------------------------------------------------------------------------------------
     dataset = SubjectLoader(subject_id=args.scene,root_fp=data_root_fp,split="train",num_rays=target_sample_batch_size // render_n_samples)
@@ -52,7 +55,7 @@ if __name__ == "__main__":
     dataset.camtoworlds = dataset.camtoworlds.to(device)
     dataset.K = dataset.K.to(device)
 
-    testPoses = SubjectTestPoseLoader(subject_id=args.scene,root_fp=data_root_fp,numberOfFrames=240)
+    testPoses = SubjectTestPoseLoader(subject_id=args.scene,root_fp=data_root_fp,numberOfFrames=8)
     testPoses.camtoworlds = testPoses.camtoworlds.to(device)
     testPoses.K = testPoses.K.to(device)
 
@@ -66,10 +69,10 @@ if __name__ == "__main__":
 
     #---------------------------------------------------------------------------------------------------------------------------------------
     contraction_type = ContractionType.AABB
-    args.aabb = [dataset.aabb[0][0], dataset.aabb[0][1], -50, dataset.aabb[1][0], dataset.aabb[1][1], 100]
+    args.aabb = [dataset.aabb[0][0], dataset.aabb[0][1], -30, dataset.aabb[1][0], dataset.aabb[1][1], 100]
     scene_aabb = torch.tensor(args.aabb, dtype=torch.float32, device=device)
     render_aabb = torch.tensor(args.aabb, dtype=torch.float32, device=device)
-    render_aabb[5] = 50
+    render_aabb[5] = 30
     
     near_plane = None
     far_plane = None
@@ -79,9 +82,9 @@ if __name__ == "__main__":
 
     #---------------------------------------------------------------------------------------------------------------------------------------
     # setup the radiance field we want to train.
-    max_steps = 50000
+    max_steps = args.max_steps
     grad_scaler = torch.cuda.amp.GradScaler(2**10)
-    radiance_field = NGPradianceField(aabb=args.aabb,unbounded=args.unbounded,).to(device)
+    radiance_field = NGPradianceField(aabb=args.aabb,unbounded=False,).to(device)
     optimizer = torch.optim.Adam(radiance_field.parameters(), lr=1e-2, eps=1e-15)
     
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
@@ -93,8 +96,79 @@ if __name__ == "__main__":
     occupancy_grid = OccupancyGrid(roi_aabb=args.aabb,resolution=grid_resolution,contraction_type=contraction_type).to(device)
     #---------------------------------------------------------------------------------------------------------------------------------------
 
-    # training
     step = 0
+    # Load checkpoints
+    args.ckpt_path = os.path.join(savepath, "ckpts")
+    load_ckpt = sorted(glob.glob(f'{args.ckpt_path}/*.ckpt'))
+    if args.ckpt_path != "" and load_ckpt != []: 
+        load_ckpt = load_ckpt[-1]
+        model = torch.load(load_ckpt)
+        step = model['step']+1
+        grad_scaler.load_state_dict(model['grad_scaler_state_dict']) # not critical
+        radiance_field.load_state_dict(model['radiance_field_state_dict'])
+        optimizer.load_state_dict(model['optimizer_state_dict'])
+        scheduler.load_state_dict(model['scheduler_state_dict']) # not critical
+        occupancy_grid.load_state_dict(model['occupancy_grid_state_dict'])
+        print(f"Loaded checkpoint from: {load_ckpt}")
+        print(f"Previous Training Loss: loss={model['loss']:.5f}")
+
+    #---------------------------------------------------------------------------------------------------------------------------------------
+    #Render the test poses only and exit...
+    if step >= 0 and args.render_only:
+        radiance_field.eval()
+        with torch.no_grad():
+            rgbs = []
+            depths = []
+            for i in tqdm.tqdm(range(len(testPoses))):
+                dataset.training = False
+                data = testPoses[i]
+                render_bkgd = data["color_bkgd"]
+                rays = data["rays"]
+
+                # rendering
+                rgb, acc, depth, _ = render_image(
+                    radiance_field,
+                    occupancy_grid,
+                    rays,
+                    render_aabb,
+                    # rendering options
+                    near_plane=near_plane,
+                    far_plane=far_plane,
+                    render_step_size=render_step_size,
+                    render_bkgd=render_bkgd,
+                    cone_angle=args.cone_angle,
+                    alpha_thre=alpha_thre,
+                    # test options
+                    test_chunk_size=args.test_chunk_size,
+                )
+                #save rgb image
+                rgbImage = (rgb.cpu().numpy() * 255).astype(np.uint8)
+                rgbs.append(rgbImage)
+                
+                #save depth image (4000 is max depth)
+                depthImage = depth.cpu().numpy()
+                depthImage = depthImage[...,-1]
+
+                # a colormap and a normalization instance
+                cmap = plt.cm.viridis
+                norm = plt.Normalize(vmin=1000, vmax=depthImage.max())
+
+                # map the normalized data to colors
+                depthImage = (cmap(norm(depthImage)) * 255).astype(np.uint8)
+                depths.append(depthImage)
+
+            rgbs = np.stack(rgbs, 0)
+            imageio.mimwrite(os.path.join(savepath,"rgb"+str(step)+".mp4"), rgbs, fps=30, quality=8)
+            
+            depths = np.stack(depths, 0)
+            imageio.mimwrite(os.path.join(savepath,"depth"+str(step)+".mp4"), depths, fps=30, quality=8)
+
+        print("All test poses are rendered! Exiting from program...")
+        exit()
+
+    #---------------------------------------------------------------------------------------------------------------------------------------
+    # training
+    # step = 0
     tic = time.time()
     for epoch in range(10000000):
         for i in range(len(dataset)):
@@ -128,7 +202,6 @@ if __name__ == "__main__":
                 
                 # compute occupancy
                 density = radiance_field.query_density(x)
-                
                 return density * step_size
 
             # update occupancy grid
@@ -160,13 +233,13 @@ if __name__ == "__main__":
 
             # compute loss
             loss = F.smooth_l1_loss(rgb[alive_ray_mask], pixels[alive_ray_mask])
-
             optimizer.zero_grad()
             # do not unscale it because we are using Adam.
             grad_scaler.scale(loss).backward()
             optimizer.step()
             scheduler.step()
 
+            #==================================================================================
             if step % 100 == 0:
                 elapsed_time = time.time() - tic
                 loss = F.mse_loss(rgb[alive_ray_mask], pixels[alive_ray_mask])
@@ -176,74 +249,35 @@ if __name__ == "__main__":
                     f"alive_ray_mask={alive_ray_mask.long().sum():d} | "
                     f"n_rendering_samples={n_rendering_samples:d} | num_rays={len(pixels):d} |"
                 )
+            
+            #==================================================================================
+            if step > 0 and step % args.i_ckpt == 0:
+                # Save checkpoint
+                ckpt_flag = True # Save flag
+                os.makedirs(args.ckpt_path, exist_ok=True)
+                ckpt_path = os.path.join(args.ckpt_path, f'model_{step}.ckpt')
+                for ckpt in sorted(glob.glob(f'{args.ckpt_path}/*.ckpt')):
+                    if int(os.path.basename(ckpt)[6:-5]) <= step or ckpt == []:
+                        os.remove(ckpt)
+                    else:
+                        print(f'Higher checkpoint is found at: {ckpt}')
+                        print('Skip saving checkpoint')
+                        ckpt_flag = False
 
-            if step >= 0 and step % args.i_test == 0 and step > 0:
-                # evaluation
-                radiance_field.eval()
+                if ckpt_flag:
+                    torch.save({
+                                'step': step,
+                                'grad_scaler_state_dict': grad_scaler.state_dict(),
+                                'radiance_field_state_dict': radiance_field.state_dict(),
+                                'optimizer_state_dict': optimizer.state_dict(),
+                                'scheduler_state_dict': scheduler.state_dict(),
+                                'occupancy_grid_state_dict': occupancy_grid.state_dict(),
+                                'loss': loss
+                                }, ckpt_path)
+                    # grad_scaler <1MB, radiance_field ~48MB, optimizer ~96MB, scheduler <1MB, occupancy_grid ~592MB
+                    print(f'Checkpoint save in: {ckpt_path}, {os.path.getsize(ckpt_path)/1024/1024:.2f}MB')
 
-                psnrs = []
-                with torch.no_grad():
-                    # for i in tqdm.tqdm(range(len(test_dataset))):
-                    rgbs = []
-                    depths = []
-                    for i in tqdm.tqdm(range(len(testPoses))):
-                        dataset.training = False
-
-                        # data = dataset[i]
-                        data = testPoses[i]
-                        render_bkgd = data["color_bkgd"]
-                        rays = data["rays"]
-                        # pixels = data["pixels"]
-
-                        # rendering
-                        rgb, acc, depth, _ = render_image(
-                            radiance_field,
-                            occupancy_grid,
-                            rays,
-                            render_aabb,
-                            # rendering options
-                            near_plane=near_plane,
-                            far_plane=far_plane,
-                            render_step_size=render_step_size,
-                            render_bkgd=render_bkgd,
-                            cone_angle=args.cone_angle,
-                            alpha_thre=alpha_thre,
-                            # test options
-                            test_chunk_size=args.test_chunk_size,
-                        )
-                        # mse = F.mse_loss(rgb, pixels)
-                        # psnr = -10.0 * torch.log(mse) / np.log(10.0)
-                        # psnrs.append(psnr.item())
-
-                        saveImg = os.path.join(savepath,"rgb_test_"+str(i)+".png")
-                        saveDepth = os.path.join(savepath,"depth_test_"+str(i)+".png")
-                        
-                        #save rgb image
-                        rgbImage = (rgb.cpu().numpy() * 255).astype(np.uint8)
-                        rgbs.append(rgbImage)
-                        
-                        #save depth image (4000 is max depth)
-                        depthImage = depth.cpu().numpy()
-                        depthImage = depthImage[...,-1]
-
-                        # a colormap and a normalization instance
-                        cmap = plt.cm.turbo
-                        norm = plt.Normalize(vmin=1000, vmax=depthImage.max())
-
-                        # map the normalized data to colors
-                        depthImage = (cmap(norm(depthImage)) * 255).astype(np.uint8)
-                        depths.append(depthImage)
-
-                    rgbs = np.stack(rgbs, 0)
-                    imageio.mimwrite(os.path.join(savepath,"rgb"+str(step)+".mp4"), rgbs, fps=30, quality=8)
-                    
-                    depths = np.stack(depths, 0)
-                    imageio.mimwrite(os.path.join(savepath,"depth"+str(step)+".mp4"), depths, fps=30, quality=8)
-
-                # psnr_avg = sum(psnrs) / len(psnrs)
-                # print(f"evaluation: psnr_avg={psnr_avg}")
-                dataset.training = True
-
+            #==================================================================================
             if step == max_steps:
                 print("training stops")
                 exit()
