09ec63c979dc0d69f93e1aa5f5beeacf2e1fa406
Ruilong Li <ruilongli94@gmail.com>
2023-01-28 23:57:30 +0000

    fix action for win

--- .github/workflows/cuda/Windows-env.sh
+++ .github/workflows/cuda/Windows-env.sh
@@ -7,11 +7,11 @@ case ${1} in
     ;;
   cu116)
     export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-    export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3/bin:${PATH}
+    export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.6/bin:${PATH}
     ;;
   cu115)
     export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-    export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3/bin:${PATH}
+    export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.5/bin:${PATH}
     ;;
   cu113)
     export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
