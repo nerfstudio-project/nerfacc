2a9c41af49de0433be00cc18358033b6f49f212d
Ruilong Li <ruilongli94@gmail.com>
2023-01-29 08:41:53 +0000

    new build with aws

--- .github/workflows/cuda/cu113-Windows.sh
+++ .github/workflows/cuda/cu113-Windows.sh
@@ -5,33 +5,11 @@
 curl -k -L "https://drive.google.com/u/0/uc?id=1injUyo3lnarMgWyRcXqKg4UGnN0ysmuq&export=download" --output "/tmp/gpu_driver_dlls.zip"
 7z x "/tmp/gpu_driver_dlls.zip" -o"/c/Windows/System32"
 
-case ${1} in
-  cu117)
-    CUDA_SHORT=11.7
-    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.1/local_installers
-    CUDA_FILE=cuda_${CUDA_SHORT}.1_516.94_windows.exe
-    ;;
-  cu116)
-    CUDA_SHORT=11.3
-    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
-    CUDA_FILE=cuda_${CUDA_SHORT}.0_465.89_win10.exe
-    ;;
-  cu115)
-    CUDA_SHORT=11.3
-    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
-    CUDA_FILE=cuda_${CUDA_SHORT}.0_465.89_win10.exe
-    ;;
-  cu113)
-    CUDA_SHORT=11.3
-    CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
-    CUDA_FILE=cuda_${CUDA_SHORT}.0_465.89_win10.exe
-    ;;
-  *)
-    echo "Unrecognized CUDA_VERSION=${1}"
-    exit 1
-    ;;
-esac
+export CUDA_SHORT=11.3
+export CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
+export CUDA_FILE=cuda_${CUDA_SHORT}.0_465.89_win10.exe
 
+# Install CUDA:
 curl -k -L "${CUDA_URL}/${CUDA_FILE}" --output "${CUDA_FILE}"
 echo ""
 echo "Installing from ${CUDA_FILE}..."
--- setup.py
+++ setup.py
@@ -1,153 +1,120 @@
-# # Environment flags to control different options
-# #
-# #   USE_MKL_BLAS=1
-# #     enables use of MKL BLAS (requires PyTorch to be built with MKL support)
-
-# import importlib
-# import os
-# import os.path as osp
-# import subprocess
-# import warnings
-
-# from setuptools import Extension, find_packages, setup
-# from setuptools.command.build_ext import build_ext
-
-# __version__ = "0.1.0"
-# URL = "https://github.com/KAIR-BAIR/nerfacc"
-
-
-# class CMakeExtension(Extension):
-#     def __init__(self, name, sourcedir=""):
-#         Extension.__init__(self, name, sources=[])
-#         self.sourcedir = os.path.abspath(sourcedir)
-
-
-# class CMakeBuild(build_ext):
-#     @staticmethod
-#     def check_env_flag(name: str, default: str = "") -> bool:
-#         value = os.getenv(name, default).upper()
-#         return value in ["1", "ON", "YES", "TRUE", "Y"]
-
-#     def get_ext_filename(self, ext_name):
-#         # Remove Python ABI suffix:
-#         ext_filename = super().get_ext_filename(ext_name)
-#         ext_filename_parts = ext_filename.split(".")
-#         ext_filename_parts = ext_filename_parts[:-2] + ext_filename_parts[-1:]
-#         return ".".join(ext_filename_parts)
-
-#     def build_extension(self, ext):
-#         import sysconfig
-
-#         import torch
-
-#         extdir = os.path.abspath(osp.dirname(self.get_ext_fullpath(ext.name)))
-#         self.build_type = "DEBUG" if self.debug else "RELEASE"
-#         if self.debug is None:
-#             if CMakeBuild.check_env_flag("DEBUG"):
-#                 self.build_type = "DEBUG"
-#             elif CMakeBuild.check_env_flag("REL_WITH_DEB_INFO"):
-#                 self.build_type = "RELWITHDEBINFO"
-
-#         if not osp.exists(self.build_temp):
-#             os.makedirs(self.build_temp)
-
-#         WITH_CUDA = torch.cuda.is_available()
-#         WITH_CUDA = bool(int(os.getenv("FORCE_CUDA", WITH_CUDA)))
-
-#         cmake_args = [
-#             "-DUSE_PYTHON=ON",
-#             f'-DWITH_CUDA={"ON" if WITH_CUDA else "OFF"}',
-#             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
-#             f"-DCMAKE_BUILD_TYPE={self.build_type}",
-#             f"-DCMAKE_PREFIX_PATH={torch.utils.cmake_prefix_path}",
-#         ]
-
-#         if CMakeBuild.check_env_flag("USE_MKL_BLAS"):
-#             include_dir = f"{sysconfig.get_path('data')}{os.sep}include"
-#             cmake_args.append(f"-DBLAS_INCLUDE_DIR={include_dir}")
-#             cmake_args.append("-DUSE_MKL_BLAS=ON")
-
-#         with_ninja = importlib.util.find_spec("ninja") is not None
-#         with_ninja |= os.environ.get("FORCE_NINJA") is not None
-#         if with_ninja:
-#             cmake_args += ["-GNinja"]
-#         else:
-#             warnings.warn(
-#                 "Building times of 'pyg-lib' can be heavily improved"
-#                 " by installing 'ninja': `pip install ninja`"
-#             )
-
-#         build_args = []
-
-#         subprocess.check_call(
-#             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
-#         )
-#         subprocess.check_call(
-#             ["cmake", "--build", "."] + build_args, cwd=self.build_temp
-#         )
-
-
-# install_requires = []
-
-# ext_modules = [CMakeExtension("libpyg")]
-# cmdclass = {"build_ext": CMakeBuild}
-
-# setup(
-#     name="nerfacc",
-#     version=__version__,
-#     description="A General NeRF Acceleration Toolbox",
-#     author="Ruilong Li",
-#     author_email="ruilongli94@gmail.com",
-#     url=URL,
-#     download_url=f"{URL}/archive/{__version__}.tar.gz",
-#     keywords=["NeRF", "acceleration", "ray tracing", "rendering"],
-#     python_requires=">=3.7",
-#     install_requires=install_requires,
-#     extras_require={},
-#     packages=find_packages(),
-#     ext_modules=ext_modules,
-#     cmdclass=cmdclass,
-# )
-
-
 import glob
+import os
 import os.path as osp
+import platform
+import sys
 
-from setuptools import setup
-
-try:
-    from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-except ImportError:
-    # Accomodate for sdist build
-    from setuptools.command.build_ext import build_ext as BuildExtension
-    from setuptools.extension import Extension as CUDAExtension
-
-ROOT_DIR = osp.dirname(osp.abspath(__file__))
+import torch
+from setuptools import find_packages, setup
+from torch.__config__ import parallel_info
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 __version__ = None
 exec(open("nerfacc/version.py", "r").read())
 
-CUDA_FLAGS = []
-INSTALL_REQUIREMENTS = []
+URL = "https://github.com/KAIR-BAIR/nerfacc"
 
-ext_modules = [
-    CUDAExtension(
-        name="nerfacc.cuda",
-        sources=glob.glob(
-            osp.join(ROOT_DIR, "nerfacc", "cuda", "csrc", "*.cu")
-        ),
-        include_dirs=[osp.join(ROOT_DIR, "nerfacc", "cuda", "csrc", "include")],
-    ),
+# VMs will fail on this check but the build will still work.
+# assert torch.cuda.is_available(), "CUDA is required to build the package"
+
+BUILD_DOCS = os.getenv("BUILD_DOCS", "0") == "1"
+WITH_SYMBOLS = os.getenv("WITH_SYMBOLS", "0") == "1"
+
+
+def get_extensions():
+
+    extensions_dir = osp.join("nerfacc", "cuda", "csrc")
+    sources = glob.glob(osp.join(extensions_dir, "*.cu"))
+    # remove generated 'hip' files, in case of rebuilds
+    sources = [path for path in sources if "hip" not in path]
+
+    undef_macros = []
+    define_macros = []
+
+    if sys.platform == "win32":
+        define_macros += [("nerfacc_EXPORTS", None)]
+
+    extra_compile_args = {"cxx": ["-O3"]}
+    if not os.name == "nt":  # Not on Windows:
+        extra_compile_args["cxx"] += ["-Wno-sign-compare"]
+    extra_link_args = [] if WITH_SYMBOLS else ["-s"]
+
+    info = parallel_info()
+    if (
+        "backend: OpenMP" in info
+        and "OpenMP not found" not in info
+        and sys.platform != "darwin"
+    ):
+        extra_compile_args["cxx"] += ["-DAT_PARALLEL_OPENMP"]
+        if sys.platform == "win32":
+            extra_compile_args["cxx"] += ["/openmp"]
+        else:
+            extra_compile_args["cxx"] += ["-fopenmp"]
+    else:
+        print("Compiling without OpenMP...")
+
+    # Compile for mac arm64
+    if sys.platform == "darwin" and platform.machine() == "arm64":
+        extra_compile_args["cxx"] += ["-arch", "arm64"]
+        extra_link_args += ["-arch", "arm64"]
+
+    nvcc_flags = os.getenv("NVCC_FLAGS", "")
+    nvcc_flags = [] if nvcc_flags == "" else nvcc_flags.split(" ")
+    nvcc_flags += ["-O3"]
+    if torch.version.hip:
+        # USE_ROCM was added to later versions of PyTorch.
+        # Define here to support older PyTorch versions as well:
+        define_macros += [("USE_ROCM", None)]
+        undef_macros += ["__HIP_NO_HALF_CONVERSIONS__"]
+    else:
+        nvcc_flags += ["--expt-relaxed-constexpr"]
+    extra_compile_args["nvcc"] = nvcc_flags
+
+    extension = CUDAExtension(
+        f"nerfacc.csrc",
+        sources,
+        include_dirs=[osp.join(extensions_dir, "include")],
+        define_macros=define_macros,
+        undef_macros=undef_macros,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+    )
+
+    return [extension]
+
+
+install_requires = []
+
+test_requires = [
+    "pytest",
+    "pytest-cov",
 ]
+
+# work-around hipify abs paths
+include_package_data = True
+if torch.cuda.is_available() and torch.version.hip:
+    include_package_data = False
 
 setup(
     name="nerfacc",
     version=__version__,
-    author="Ruilong Li",
-    author_email="ruilongli94@gmail.com",
-    description="A General NeRF Acceleration Toolbox",
-    ext_modules=ext_modules,
-    setup_requires=["pybind11>=2.5.0"],
-    packages=["nerfacc", "nerfacc.cuda"],
-    cmdclass={"build_ext": BuildExtension},
+    description="",
+    author="",
+    author_email="",
+    url=URL,
+    download_url=f"{URL}/archive/{__version__}.tar.gz",
+    keywords=[],
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    extras_require={
+        "test": test_requires,
+    },
+    ext_modules=get_extensions() if not BUILD_DOCS else [],
+    cmdclass={
+        "build_ext": BuildExtension.with_options(
+            no_python_abi_suffix=True, use_ninja=False
+        )
+    },
+    packages=find_packages(),
+    include_package_data=include_package_data,
 )
