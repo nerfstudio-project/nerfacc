a3d271931d5db75dd1ea063cc7445c149197e4a7
Ruilong Li <ruilongli94@gmail.com>
2023-03-30 00:54:32 +0000

    pack.py and test

--- nerfacc/pack.py
+++ nerfacc/pack.py
@@ -4,12 +4,32 @@ import torch
 from torch import Tensor
 
 
+@torch.no_grad()
 def pack_info(ray_indices: Tensor, n_rays: Optional[int] = None) -> Tensor:
-    """Pack ray indices."""
-    device = ray_indices.device
-    if n_rays is None:
-        n_rays = ray_indices.max().item() + 1
-    chunk_cnts = torch.zeros((n_rays,), device=device, dtype=torch.long)
-    chunk_cnts.scatter_add_(0, ray_indices, torch.ones_like(ray_indices))
-    chunk_starts = chunk_cnts.cumsum(dim=0, dtype=torch.long) - chunk_cnts
-    return chunk_starts, chunk_cnts
+    """Pack `ray_indices` to `packed_info`. Useful for converting per sample data to per ray data.
+
+    Note:
+        this function is not differentiable to any inputs.
+
+    Args:
+        ray_indices: Ray index of each sample. LongTensor with shape (n_sample).
+
+    Returns:
+        packed_info: A LongTensor of shape (n_rays, 2) that specifies the start and count
+            of each chunk in the flattened input tensor, with in total n_rays chunks.
+    """
+    assert (
+        ray_indices.dim() == 1
+    ), "ray_indices must be a 1D tensor with shape (n_samples)."
+    if ray_indices.is_cuda:
+        device = ray_indices.device
+        dtype = ray_indices.dtype
+        if n_rays is None:
+            n_rays = ray_indices.max().item() + 1
+        chunk_cnts = torch.zeros((n_rays,), device=device, dtype=dtype)
+        chunk_cnts.index_add_(0, ray_indices, torch.ones_like(ray_indices))
+        chunk_starts = chunk_cnts.cumsum(dim=0, dtype=dtype) - chunk_cnts
+        packed_info = torch.stack([chunk_starts, chunk_cnts], dim=-1)
+    else:
+        raise NotImplementedError("Only support cuda inputs.")
+    return packed_info
--- tests/test_pack.py
+++ tests/test_pack.py
@@ -1,42 +1,22 @@
 import pytest
 import torch
 
-from nerfacc import pack_data, pack_info, unpack_data, unpack_info
-
 device = "cuda:0"
-batch_size = 32
-eps = 1e-6
 
 
 @pytest.mark.skipif(not torch.cuda.is_available, reason="No CUDA device")
-def test_pack_data():
-    n_rays = 2
-    n_samples = 3
-    data = torch.rand((n_rays, n_samples, 2), device=device, requires_grad=True)
-    mask = torch.rand((n_rays, n_samples), device=device) > 0.5
-    packed_data, packed_info = pack_data(data, mask)
-    unpacked_data = unpack_data(packed_info, packed_data, n_samples)
-    unpacked_data.sum().backward()
-    assert (data.grad[mask] == 1).all()
-    assert torch.allclose(
-        unpacked_data.sum(dim=1), (data * mask[..., None]).sum(dim=1)
-    )
+def test_pack_info():
+    from nerfacc.pack import pack_info
 
-
-@pytest.mark.skipif(not torch.cuda.is_available, reason="No CUDA device")
-def test_unpack_info():
-    packed_info = torch.tensor(
+    _packed_info = torch.tensor(
         [[0, 1], [1, 0], [1, 4]], dtype=torch.int32, device=device
     )
-    ray_indices_tgt = torch.tensor(
+    ray_indices = torch.tensor(
         [0, 2, 2, 2, 2], dtype=torch.int64, device=device
     )
-    ray_indices = unpack_info(packed_info, n_samples=5)
-    packed_info_2 = pack_info(ray_indices, n_rays=packed_info.shape[0])
-    assert torch.allclose(packed_info.int(), packed_info_2.int())
-    assert torch.allclose(ray_indices, ray_indices_tgt)
+    packed_info = pack_info(ray_indices, n_rays=_packed_info.shape[0])
+    assert (packed_info == _packed_info).all()
 
 
 if __name__ == "__main__":
-    test_pack_data()
-    test_unpack_info()
+    test_pack_info()
