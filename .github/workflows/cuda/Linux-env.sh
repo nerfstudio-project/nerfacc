627d44465ea30f66bb9c62d91230043607fdb811
Ruilong Li(李瑞龙) <ruilongli94@gmail.com>
2023-04-05 23:04:53 -0700

    Workflow (#192)
    * update workflow
    
    * tiny fix
    
    * format
    
    * format
    
    * fix workflow
    
    * fix workflow
    
    * update cuda env file

--- .github/workflows/cuda/Linux-env.sh
+++ .github/workflows/cuda/Linux-env.sh
@@ -5,21 +5,33 @@
 case ${1} in
   cu118)
     export PATH=/usr/local/cuda-11.8/bin:${PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:${LD_LIBRARY_PATH}
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
     ;;
   cu117)
     export PATH=/usr/local/cuda-11.7/bin:${PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64:${LD_LIBRARY_PATH}
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
     ;;
   cu116)
     export PATH=/usr/local/cuda-11.6/bin:${PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64:${LD_LIBRARY_PATH}
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
     ;;
   cu115)
     export PATH=/usr/local/cuda-11.5/bin:${PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.5/lib64:${LD_LIBRARY_PATH}
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
     ;;
   cu113)
     export PATH=/usr/local/cuda-11.3/bin:${PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64:${LD_LIBRARY_PATH}
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
     ;;
   cu102)
     export PATH=/usr/local/cuda-10.2/bin:${PATH}
+    export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64:${LD_LIBRARY_PATH}
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5"
     ;;
   *)
     ;;
--- .github/workflows/cuda/Windows-env.sh
+++ .github/workflows/cuda/Windows-env.sh
@@ -5,18 +5,23 @@
 case ${1} in
   cu118)
     export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.8/bin:${PATH}
+    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu117)
     export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.7/bin:${PATH}
+    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu116)
     export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3/bin:${PATH}
+    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu115)
     export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3/bin:${PATH}
+    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu113)
     export PATH=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3/bin:${PATH}
+    export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   *)
     ;;
