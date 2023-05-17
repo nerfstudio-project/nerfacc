46a22cc03bbc87f5d04849dbb8356e052788bea6
Ruilong Li <ruilongli94@gmail.com>
2023-05-17 19:04:43 +0000

    fix

--- .github/workflows/cuda/Windows-env.sh
+++ .github/workflows/cuda/Windows-env.sh
@@ -4,33 +4,33 @@
 
 case ${1} in
   cu118)
-    export CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.8
-    export PATH=${CUDA_HOME}/bin:$PATH
-    export PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+    CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.8
+    PATH=${CUDA_HOME}/bin:$PATH
+    PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
     export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu117)
-    export CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.7
-    export PATH=${CUDA_HOME}/bin:$PATH
-    export PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+    CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.7
+    PATH=${CUDA_HOME}/bin:$PATH
+    PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
     export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu116)
-    export CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.6
-    export PATH=${CUDA_HOME}/bin:$PATH
-    export PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+    CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.6
+    PATH=${CUDA_HOME}/bin:$PATH
+    PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
     export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu115)
-    export CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.5
-    export PATH=${CUDA_HOME}/bin:$PATH
-    export PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+    CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.5
+    PATH=${CUDA_HOME}/bin:$PATH
+    PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
     export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   cu113)
-    export CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3
-    export PATH=${CUDA_HOME}/bin:$PATH
-    export PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+    CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3
+    PATH=${CUDA_HOME}/bin:$PATH
+    PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
     export TORCH_CUDA_ARCH_LIST="6.0+PTX"
     ;;
   *)
